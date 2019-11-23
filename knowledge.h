9b7af179c60bd851cbf7a6d9882516c4c5367ec3
Zhuo Yue <zyue17@gmail.com>
2019-11-23 15:04:09 +0800

    Amended knowledge_read and knowledge_write's declarations under knowledge.h

--- knowledge.h
+++ knowledge.h
@@ -34,5 +34,5 @@ void knowledge_init();
 int knowledge_get(const char* intent, const char* entity, char* response, int n);
 int knowledge_put(const char* intent, const char* entity, const char* response);
 void knowledge_reset();
-int knowledge_read(FILE* f);
-void knowledge_write(FILE* f);
+void knowledge_read();
+int knowledge_write(char* pacPath, char* pacSection, char* pacKey, char* pacValue);
